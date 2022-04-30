7c196e181c1f2d3b1554798cd70dd0108d30f973
adithya-1 <adithyaaravi10@gmail.com>
2022-04-30 22:27:00 +0530

    First commit

---
+++ integration/ReadME.md
@@ -0,0 +1,30 @@
+Dataset
+    DL-SampleSentences.csv- Contains the enitre dataset
+    References.csv- Contains file name correspoding to each classes
+Splitting
+    Splitting.ipynb - Python notebook file to split audio files to unique words which takes input as full sentences and splits them into individual words
+    
+    demoRecording.wav- demo input file
+
+Preprocess
+    preprocessAudio.ipynb- Python notebook file used for preprocessing
+
+WordPredictor
+    modelOne- Word Predictor model using uni-gram
+    modelTwo- Word Predictor model using bi-gram
+    modelThree- Word Predictor model using tri-gram
+    baseWordPredictor.ipynb - Python Notebook file used to create n-gram models
+    doc3.py- Sentences used to build the model
+
+DLModel
+    DLmodel.ipynb -  Python Notebook file of deep learning model
+
+Similarity
+    SimilarityModel.ipynb -  Python Notebook of smilarity model
+
+Integration 
+    Integrates all the above specified models. Download this folder and run DlModel.ipynb and similarity.ipynb to test the models
+    
+
+
+
---
+++ wordPredictor/doc3.py
@@ -0,0 +1,31 @@
+training_doc3 = """
+<s> nanna next turn yenu <e>
+<s> what is my next saradi <e>
+<s> what is my mundina saradi <e>
+<s> how is the datthane ahead <e>
+<s> munde traffic hegidhe <e>
+<s> how is the datthane munde <e>
+<s> what are the nearby anila stations <e>
+<s> what are the nearby gas kendragalu <e>
+<s> what are the nearby anila kendragalu <e>
+<s> hathira gas stations yavdu <e>
+<s> hathira gas kendragalu yavdu <e>
+<s> hathira anila stations yavdu <e>
+<s> are there any kredaa mydana near me <e>
+<s> nanna hathira yavude sports ground idhya <e>
+<s> are there any sports mydana near me <e>
+<s> are there any kredaa grounds near me <e>
+<s> what are some good nearby stalagalu to visit <e>
+<s> what are some uttama hathiradha places to visit <e>
+<s> what are some uttama hathiradha stalagalu to visit <e>
+<s> bheti needalu hathiradha good places yavuvu <e>
+<s> hathiradha park yaavaaga thereyuthadhe <e>
+<s> When does the nearest udhyaana open <e>
+<s> hathiradha udhyaanavu yaavaaga open aagothe <e>
+<s> Nanna hathira iro plumbing service <e>
+<s> Hathira iro kolaayi service <e>
+<s> Hathira iro plumbing service <e>
+<s> hathiradha job openings <e>
+<s> Udhyogavakaasha near me <e>
+<s> Kelasa openings near me <e>
+"""
